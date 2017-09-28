fc5222b3653ec5f1490b74dc8a0668b5bdaceca4
mcproger <mcproger@localhost.localdomain>
2017-09-28 23:47:37 +0300

    Add business logic

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,49 @@
 import vk
-
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+import getpass
+import time
 
 
 def get_user_login():
-    pass
+    user_login = input('Enter login from vk.com: ')
+    return user_login
 
 
 def get_user_password():
-    pass
+    user_password = getpass.getpass('Enter password from vk.com: ')
+    return user_password
 
 
-def get_online_friends(login, password):
+def get_vk_api_session(APP_ID, login, password):
     session = vk.AuthSession(
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
     api = vk.API(session)
-    # например, api.friends.get()
+    return api
 
 
-def output_friends_to_console(friends_online):
-    pass
+def get_online_friends_id_list(vk_api):
+    online_friends_id_list = vk_api.friends.getOnline()
+    return online_friends_id_list
+
+
+def output_friends_to_console(vk_api, online_friend_id):
+    online_friend_info = vk_api.users.get(user_ids=online_friend_id)
+    time.sleep(0.5) # Fix too many requests exception
+    online_friend = '{first_name} {last_name}'.format(
+        first_name=online_friend_info[0]['first_name'],
+        last_name=online_friend_info[0]['last_name']
+    )
+    return online_friend
+
 
 if __name__ == '__main__':
+    APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    vk_api = get_vk_api_session(APP_ID, login, password)
+    online_friends_id_list = get_online_friends_id_list(vk_api)
+    for online_friend_id in online_friends_id_list:
+        print(output_friends_to_console(vk_api, online_friend_id))
