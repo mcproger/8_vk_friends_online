7150ef7db8bc858a04a921ed48fb122ed038ca64
mcproger <mcproger@localhost.localdomain>
2017-10-07 20:24:52 +0300

    Optimaze fetch of data via vk api and function decomposition

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -32,19 +32,12 @@ def get_vk_api_session(app_id, login, password):
     return api
 
 
-def output_friends_to_console(online_friend_info):
+def output_friend_to_console(online_friend_info):
     online_friend = '{first_name} {last_name}'.format(
-        first_name=online_friend_info[0]['first_name'],
-        last_name=online_friend_info[0]['last_name'],
+        first_name=online_friend_info['first_name'],
+        last_name=online_friend_info['last_name'],
     )
     return online_friend
-
-
-def get_online_friend_info(vk_api, online_friend_id):
-    online_friend_info = vk_api.users.get(user_ids=online_friend_id)
-    too_many_requests_time_delay = 0.3
-    time.sleep(too_many_requests_time_delay)
-    return online_friend_info
 
 
 if __name__ == '__main__':
@@ -54,6 +47,6 @@ if __name__ == '__main__':
     password = get_user_password()
     vk_api = get_vk_api_session(app_id, login, password)
     online_friends_id_list = vk_api.friends.getOnline()
-    for online_friend_id in online_friends_id_list:
-        online_friend_info = get_online_friend_info(vk_api, online_friend_id)
-        print(output_friends_to_console(online_friend_info))
+    online_friends_info = vk_api.users.get(user_ids=online_friends_id_list)
+    for online_friend_info in online_friends_info:
+        print(output_friend_to_console(online_friend_info))
